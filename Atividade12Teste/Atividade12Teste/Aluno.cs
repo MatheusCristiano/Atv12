7ded62011024fa78cfe4a1dca8ac18b8e08d7654
MatheusCristiano <36750514+MatheusCristiano@users.noreply.github.com>
2018-06-11 22:20:58 -0300

    atividade

---
+++ Atividade12Teste/Atividade12Teste/Aluno.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade12Teste
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public string Snome /*sobrenome*/ { get; set; }
+        public DateTime DataNas { get; set; }
+        public int Codigo { get; set; }
+
+        public DateTime Data;
+
+        public int Idade()
+        {
+            var b = DateTime.Now;
+            Data = b;
+            var i = Convert.ToInt32(Data.Year - DataNas.Year);
+            return i;
+        }
+
+        public string NameComp()
+        {
+            return "" + Nome + " " + Snome + "";
+        }
+
+        public int validarNome()
+        {
+            int i = 0;
+            string n = Nome;
+            string s = Snome;
+
+            if (n.Contains(" Ç") || n.Contains(" ç") || s.Contains(" Ç") || s.Contains(" ç"))
+            {
+                Console.WriteLine("Este nome é não pode ser utilizado pois tem a letra 'Ç' que é inválido");
+                i = +1;
+            }
+            if (n.Contains("@") || s.Contains("@"))
+            {
+                Console.WriteLine("Este nome não podeser utilizado pois contém caracter '@' que é inválido");
+                i = +2;
+            }
+            return i;
+        }
+
+        public string ErroVal(int i)
+        {
+            string m;
+            if (validarNome() == 1)
+            {
+                m = "ERROR POR 'Ç'";
+                return m;
+            }
+            else if (validarNome() == 2)
+            {
+                m = "ERROR POR '@'";
+                return m;
+            }
+
+            return " ";
+        }
+    }
+}
---
+++ Atividade12Teste/Atividade12Teste/UnitTest1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Atividade12Teste;
+
+namespace Teste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Aluno a1 = new Aluno();
+        Aluno a2 = new Aluno();
+        Aluno a3 = new Aluno();
+
+        [TestMethod]
+        public void Idade1()
+        {
+            a1.DataNas = new DateTime(1990, 04, 01);
+
+            Assert.IsTrue(a1.Idade() > 0);
+        }
+        [TestMethod]
+        public void Idade2()
+        {
+            a2.DataNas = new DateTime(2020, 01, 01);
+
+            Assert.IsTrue(a2.Idade() > 0);
+        }
+        [TestMethod]
+        public void Idade3()
+        {
+            a3.DataNas = new DateTime(1986, 03, 12);
+
+            Assert.IsTrue(a3.Idade() > 0);
+        }
+
+        [TestMethod]
+        public void nome1()
+        {
+            a1.Nome = "Felipe";
+            a1.Snome = "Vozgeral Çanca";
+
+            Assert.IsTrue(a1.validarNome() != 0);
+        }
+        [TestMethod]
+        public void nome2()
+        {
+            a2.Nome = "Alin@";
+            a2.Snome = "Branca";
+
+            Assert.IsFalse(a2.validarNome() != 0);
+        }
+        [TestMethod]
+        public void nome3()
+        {
+            a3.Nome = "Alfredo";
+            a3.Snome = "Sella III";
+
+            Assert.IsTrue(a3.validarNome() != 0);
+        }
+
+        [TestMethod]
+        public void nomeC1()
+        {
+            a1.Nome = "Felipe";
+            a2.Snome = "Vozgeral Çanca";
+
+            Assert.AreEqual(("Felipe Vozgeral Çanca"), a1.NameComp());
+        }
+        [TestMethod]
+        public void nomeC2()
+        {
+            a2.Nome = "Alin@";
+            a2.Snome = "Branca";
+
+            Assert.AreEqual(("Aline Branca"), a2.NameComp());
+        }
+
+        [TestMethod]
+        public void erro1()
+        {
+            a1.Nome = "Felipe";
+            a1.Snome = "Vozgeral Çanca";
+
+            a1.validarNome();
+            Assert.AreEqual("ERROR POR '@'", a1.ErroVal(a1.validarNome()));
+        }
+
+        [TestMethod]
+        public void erro3()
+        {
+            a3.Nome = "Alfredo";
+            a3.Snome = "Sella III";
+
+            a3.validarNome();
+            Assert.AreEqual("ERROR POR 'Ç'", a3.ErroVal(a3.validarNome()));
+        }
+    }
+}
